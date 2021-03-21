beb0e1a2a9be89258db6d6d6a888aea3c242691a
Katherine Tam <katherinemtam@gmail.com>
2021-03-20 19:35:40 -0700

    add dropdown functionality

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-import { handleAddClick, handleSubClick, handleMultClick, handleDivClick } from './handler.js';
+import { handleAddClick, handleSubClick, handleMultClick, handleDivClick, handleEverythingClick } from './handler.js';
 
 const addButton = document.getElementById("add-button");
 const subButton = document.getElementById("sub-button");
 const multButton = document.getElementById("mult-button");
 const divButton = document.getElementById("div-button");
+const calculateButton = document.getElementById("calculate-button");
 
-addButton.addEventListener('click', handleAddClick)
-subButton.addEventListener('click', handleSubClick)
-multButton.addEventListener('click', handleMultClick)
-divButton.addEventListener('click', handleDivClick)
+addButton.addEventListener('click', handleAddClick);
+subButton.addEventListener('click', handleSubClick);
+multButton.addEventListener('click', handleMultClick);
+divButton.addEventListener('click', handleDivClick);
+calculateButton.addEventListener('click', handleEverythingClick);
